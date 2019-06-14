60a6a4c48d4245f20c043c7a5e1289bd108b4cf3
luotao <luotao>
2019-06-14 09:39:50 +0800

    完成

---
+++ src/pages/listcate.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div>
+    <ListCate_List v-for="(item,index) in musicTypeJSON" :musicType="item" :key="index"/>
+  </div>
+</template>
+
+<script>
+
+import MusicType from "../assets/data/musictype.json"
+import ListCate_List from "../components/ListCate_List"
+
+export default {
+  data(){
+   return{
+     musicTypeJSON:[]
+   }
+  },
+  components:{
+    ListCate_List
+  },
+  created(){
+    this.musicTypeJSON = MusicType.currentType
+  }
+}
+</script>
+
+<style lang="css">
+</style>
